e800cef0570390f08b4aa3e87bfc132d92aacdfe
Davidager <david.malmstrm@gmail.com>
2017-02-14 21:26:03 +0100

    Database tillagt!

---
+++ Database/Database/Assets/Scripts/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+public class Class1
+{
+	public Class1()
+	{
+        string[] stringArray = new string[10];
+        Console.WriteLine("ffyyfth");
+        Console.WriteLine(stringArray[1]);
+        //Console.WriteLine(stringArray[1].GetType());
+        stringArray[7] = null;
+        //Console.ReadKey();
+	}
+
+    static void Main()
+    {
+        new Class1();
+    }
+}
