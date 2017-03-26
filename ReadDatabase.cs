45d2ffa4211204240ecae82a25518189e2728023
stekaa95 <stefan.kaalen@hotmail.com>
2017-03-26 20:29:24 +0200

    Add files via upload

---
+++ ReadDatabase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+
+
+public class ReadDatabase{
+
+
+    public ExampleContainer exampleContainer;
+    public ReadDatabase()
+    {
+       
+        public XmlSerializer serializer = new XmlSerializer(typeof(ExampleContainer));
+        public Stream filereader = new FileStream(@"D:\KEX\Kex - master\Database", FileMode.Open);
+        exampleContainer = serializer.Deserialize(filereader) as ExampleContainer;
+        filereader.Close();
+    }
+
+	// Update is called once per frame
+}
+
+    //https://msdn.microsoft.com/en-us/library/dsh84875(v=vs.110).aspx
+
